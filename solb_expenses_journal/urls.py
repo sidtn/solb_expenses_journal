4cdeb115d8b80c75e83d8609fef0b0f65af54f61
apiven <sidtnlol@gmail.com>
2022-06-22 13:19:29 +0300

    add jwt

--- solb_expenses_journal/urls.py
+++ solb_expenses_journal/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from journal_api.views import CreateUserView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", CreateUserView.as_view(), name="register"),
     path("api/v1/", include("journal_api.urls")),
 ]
