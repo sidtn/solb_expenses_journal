0c37d2408f6423ac55a35dad7622c1ef25db94d2
apiven <sidtnlol@gmail.com>
2022-06-23 16:37:22 +0300

    change pk category step 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,9 @@ ADMIN_USERNAME=${ADMIN_USERNAME}
 ADMIN_EMAIL=${ADMIN_EMAIL}
 ADMIN_PASSWORD=${ADMIN_PASSWORD}
 
-./manage.py makemigrations
 ./manage.py migrate
 ./manage.py loaddata categories.json
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(username='$ADMIN_USERNAME', email='$ADMIN_EMAIL', password='$ADMIN_PASSWORD')" | ./manage.py shell
 
-./manage.py runserver 0.0.0.0:8000
+./manage.py runserver 0.0.0.0:8000 --noreload
