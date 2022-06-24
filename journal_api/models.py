30567f5bd72cff92fa191da2f85e8d1c40f9ba41
apiven <sidtnlol@gmail.com>
2022-06-24 17:43:10 +0300

    change_pk_category_to_uuid_step_3

--- journal_api/models.py
+++ journal_api/models.py
@@ -1,8 +1,7 @@
 import uuid as uuid
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db import models, connection
 from django.utils.translation import gettext as _
-
 from journal_api.core.validators import validate_positive
 
 
@@ -19,13 +18,22 @@ class Category(models.Model):
     uuid = models.UUIDField(
         max_length=36,
         default=uuid.uuid4,
-        unique=True,
+        primary_key=True,
         editable=False
     )
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True, related_name="categories"
     )
     name = models.CharField(max_length=100, verbose_name="Expense category")
+    id = models.IntegerField(editable=False)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            last_id = Category.objects.all().aggregate(largest=models.Max('id'))['largest']
+            if last_id is not None:
+                self.id = last_id + 1
+
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         unique_together = ("owner", "name")
