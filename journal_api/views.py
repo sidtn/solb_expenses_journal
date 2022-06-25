a0731680fda10135ed6e895b078969602114ce52
sidtn <sidtnlol@gmail.com>
2022-06-25 12:00:40 +0300

    old api support

--- journal_api/views.py
+++ journal_api/views.py
@@ -48,6 +48,24 @@ class ExpenseAPIViewSet(viewsets.ModelViewSet):
             return Expense.objects.filter(owner=user)
         return Expense.objects.all()
 
+    def create(self, request, *args, **kwargs):
+
+        # old api support --------------------------------
+        if request.data["category"].isdigit():
+            try:
+                category_id = int(request.data["category"])
+                category_uuid = Category.objects.values("uuid").get(id=category_id)["uuid"]
+                request.data._mutable = True
+                request.data.update({"category": category_uuid})
+            except Category.DoesNotExist:
+                pass
+        # ------------------------------------------------
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     @action(url_path="total", methods=["GET"], detail=False)
     def total_expenses(self, request):
         user = self.request.user
