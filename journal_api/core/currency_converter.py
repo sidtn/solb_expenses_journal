936e323b1cfd5e48dedde60f4e1b068ab328b635
Anatoli Piven <sidtnlol@gmail.com>
2022-06-29 00:52:56 +0300

    another exchange rates service

--- journal_api/core/currency_converter.py
+++ journal_api/core/currency_converter.py
@@ -1,4 +1,5 @@
 import os
+from decimal import Decimal
 
 import requests
 from dotenv import load_dotenv
@@ -11,12 +12,15 @@ class BadResponseFromCurrencyAPI(Exception):
 
 
 def currency_converter(from_currency, to_currency, amount):
-    headers = {"apikey": os.getenv("API_KEY_CURRENCY")}
-    url = f"https://api.apilayer.com/currency_data/convert?to={to_currency}&from={from_currency}&amount={amount}"
-    response = requests.request("GET", url, headers=headers)
+    # headers = {"apikey": os.getenv("API_KEY_CURRENCY")}
+    # url = f"https://api.apilayer.com/currency_data/convert?to={to_currency}&from={from_currency}&amount={amount}"
+    # response = requests.request("GET", url, headers=headers)
+    url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}"
+    response = requests.get(url)
     result = response
     try:
-        return result.json()["result"]
+        rate = result.json()["result"]
+        return Decimal(rate) * amount
     except KeyError:
         print(result.json())
         raise BadResponseFromCurrencyAPI(
