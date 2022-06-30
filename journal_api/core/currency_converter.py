b2f3b810d48daa80e074ef7b7b04fce4c6a9e13b
apiven <sidtnlol@gmail.com>
2022-06-30 20:37:22 +0300

    add options to call decorator

--- journal_api/core/currency_converter.py
+++ journal_api/core/currency_converter.py
@@ -11,28 +11,34 @@ class BadResponseFromCurrencyAPI(Exception):
 class CurrencyRatesCache:
 
     """
-    Takes named arguments in timedelta format.
-    example: minutes=1, seconds=10
+    Takes named argument storage_time in seconds.
+    Default: 20 seconds
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, func=None, storage_time=20):
         self.cache = {}
-        self.storage_time = kwargs
+        self._storage_time = storage_time
+        self._func = func
 
-    def __call__(self, get_currency_rate_function):
-        def get_rates(from_currency, to_currency):
-            cached_rate = self.cache.get(f"{from_currency} - {to_currency}")
-            if cached_rate and datetime.datetime.now() - cached_rate.get("created") < datetime.timedelta(**self.storage_time):
-                rate = cached_rate.get("rate")
-                return rate
-            else:
-                rate = get_currency_rate_function(from_currency, to_currency)
-                self.cache[f"{from_currency} - {to_currency}"] = {"rate": rate, "created": datetime.datetime.now()}
-                return rate
-        return get_rates
+    def get_rates(self, from_currency, to_currency):
+        cached_rate = self.cache.get(f"{from_currency} - {to_currency}")
+        if cached_rate and datetime.datetime.now() - cached_rate.get("created") < datetime.timedelta(
+                seconds=self._storage_time):
+            rate = cached_rate.get("rate")
+            return rate
+        else:
+            rate = self._func(from_currency, to_currency)
+            self.cache[f"{from_currency} - {to_currency}"] = {"rate": rate, "created": datetime.datetime.now()}
+            return rate
+
+    def __call__(self, *args):
+        if callable(args[0]):
+            self._func = args[0]
+            return self.get_rates
+        return self.get_rates(*args)
 
 
-@CurrencyRatesCache(seconds=10)
+@CurrencyRatesCache
 def currency_converter(from_currency, to_currency):
     url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}"
     response = requests.get(url)
