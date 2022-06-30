a8320a249eca1cf32f0c070ee846f5ea7a887e13
apiven <sidtnlol@gmail.com>
2022-06-30 13:12:53 +0300

    command fix_id_category optimization

--- journal_api/management/commands/fix_id_category.py
+++ journal_api/management/commands/fix_id_category.py
@@ -7,17 +7,17 @@ from journal_api.models import Category
 
 class Command(BaseCommand):
     def handle(self, **options):
-        list_duplicate_objects = []
+        list_duplicated_objects = []
         duplicates_id = Category.objects.values('id').annotate(Count('id')) .order_by("id").filter(id__count__gt=1)
         for data in duplicates_id:
             duplicates = Category.objects.filter(id=data["id"])[:data["id__count"] - 1]
             for cat in duplicates:
-                list_duplicate_objects.append(cat)
+                list_duplicated_objects.append(cat)
         with lock_table(Category):
             last_id = Category.objects.all().aggregate(
                 largest=Max("id")
             )["largest"]
-            for id_index, obj in enumerate(list_duplicate_objects, start=1):
+            for id_index, obj in enumerate(list_duplicated_objects, start=1):
                 obj.id = last_id + id_index
-            Category.objects.bulk_update(list_duplicate_objects, ["id"])
+            Category.objects.bulk_update(list_duplicated_objects, ["id"])
 
