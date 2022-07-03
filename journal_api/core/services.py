78d9ba6a2a795e7570920b083c0209c20ca91441
sidtn <sidtnlol@gmail.com>
2022-07-03 20:34:10 +0300

    total expenses endpoint

--- journal_api/core/services.py
+++ journal_api/core/services.py
@@ -1,19 +1,36 @@
 import datetime
+
 from journal_api.core.currency_converter import (
     BadResponseFromCurrencyAPI,
     currency_converter,
 )
-from journal_api.models import Expense
+from journal_api.models import Category, Expense
 
 
 class TotalExpenses:
-
     def __init__(self, request):
         self.user = request.user
         self.category = request.query_params.get("category")
         self.start_date = request.query_params.get("start_date")
         self.end_date = request.query_params.get("end_date")
         self.currency = request.query_params.get("currency")
+
+    @staticmethod
+    def create_exp_dict(expense):
+        cat_exp_dict = {
+            "category_uuid": expense.category.uuid,
+            "category_name": expense.category.name,
+            "cat_expenses": [],
+        }
+        return cat_exp_dict
+
+    @staticmethod
+    def get_parent_name(category):
+        category_name = ""
+        ancestors = category.get_ancestors(include_self=True)
+        for cat in ancestors:
+            category_name += f"{cat.name}, "
+        return category_name.rstrip(", ")
 
     def get_filter_dict(self):
         if not self.start_date:
@@ -24,52 +41,58 @@ class TotalExpenses:
             "created_at__range": [self.start_date, self.end_date],
         }
         if self.category:
-            filter_dict["category"] = self.category
+            filter_dict["category__in"] = Category.objects.get(
+                uuid=self.category
+            ).get_descendants(include_self=True)
         else:
             self.category = "all"
 
         return filter_dict
 
-    def create_exp_dict(self, expense):
-        exp_dict = {
-            "date": expense.created_at,
-            "category_uuid": expense.category.uuid,
-            "category_name": expense.category.name,
-            expense.currency.code: expense.amount,
-        }
-        if self.currency:
-            try:
-                in_request_currency = round(currency_converter(expense.currency.code, self.currency) * expense.amount, 2)
-            except BadResponseFromCurrencyAPI:
-                in_request_currency = None
-            exp_dict[f"in_{self.currency}"] = in_request_currency
-        exp_dict["children"] = []
-        return exp_dict
-
     def get_report(self):
         expenses = (
-            Expense.objects.filter(owner__id=self.user.id, **self.get_filter_dict())
+            Expense.objects.filter(
+                owner__id=self.user.id, **self.get_filter_dict()
+            )
         ).order_by("category__level")
         report = {
             "start_date": self.start_date,
             "end_date": self.end_date,
             "category": self.category,
-            "expenses": []
+            "expenses": [],
         }
+        showed_cat = []
         for exp in expenses:
-            exp_dict = self.create_exp_dict(exp)
-            report["expenses"].append(exp_dict)
-            for i in expenses:
-                if i.category in exp.category.get_children():
-                    d = self.create_exp_dict(i)
-                    exp_dict["children"].append(d)
+            if exp.category not in showed_cat:
+                family_expenses = expenses.filter(
+                    category__in=exp.category.get_descendants(
+                        include_self=True
+                    )
+                )
+                exp_dict = self.create_exp_dict(exp)
+                for f_exp in family_expenses:
+                    sub_cat_exp_dict = {
+                        "date": f_exp.created_at,
+                        "category_uuid": f_exp.category.uuid,
+                        "category_name": f_exp.category.name
+                        if f_exp.category.get_level() == 0
+                        else self.get_parent_name(f_exp.category),
+                        "short_description": f_exp.short_description,
+                        f_exp.currency.code: f_exp.amount,
+                    }
+                    exp_dict["cat_expenses"].append(sub_cat_exp_dict)
+                    if self.currency:
+                        try:
+                            sub_cat_exp_dict[f"in_{self.currency}"] = round(
+                                f_exp.amount
+                                * currency_converter(
+                                    f_exp.currency.code, self.currency
+                                ),
+                                2,
+                            )
+                        except BadResponseFromCurrencyAPI:
+                            sub_cat_exp_dict[f"in_{self.currency}"] = None
+                    showed_cat.append(f_exp.category)
+                report["expenses"].append(exp_dict)
 
         return report
-
-
-
-
-
-
-
-
