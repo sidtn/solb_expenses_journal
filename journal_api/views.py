43ba96b1a0897099ba7830bab92ac01bd873fbd1
apiven <sidtnlol@gmail.com>
2022-07-06 12:36:55 +0300

    add query parameters to swagger and lookup by category name

--- journal_api/views.py
+++ journal_api/views.py
@@ -1,4 +1,6 @@
 from django.db.models import Q
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView
@@ -21,6 +23,14 @@ class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
 
 
+name = openapi.Parameter(
+    "name",
+    in_=openapi.IN_QUERY,
+    description="Category name",
+    type=openapi.TYPE_STRING,
+)
+
+
 class CategoryAPIViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -34,6 +44,22 @@ class CategoryAPIViewSet(viewsets.ModelViewSet):
             user = self.request.user
             return Category.objects.filter(Q(owner=user) | Q(owner=None))
         return Category.objects.filter(owner=None)
+
+    @swagger_auto_schema(manual_parameters=[name])
+    def list(self, request, *args, **kwargs):
+        if request.query_params.get("name"):
+            queryset = self.filter_queryset(self.get_queryset()).filter(
+                name__icontains=request.query_params.get("name")
+            )
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class ExpenseAPIViewSet(viewsets.ModelViewSet):
@@ -72,6 +98,10 @@ class ExpenseAPIViewSet(viewsets.ModelViewSet):
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
 
+    @swagger_auto_schema(
+        query_serializer=TotalExpensesSerializer,
+        operation_id="Get total expenses endpoint",
+    )
     @action(url_path="total", methods=["GET"], detail=False)
     def total_expenses(self, request):
         qp = TotalExpensesSerializer(data=request.query_params)
